83da75b40d89d64d0c3b6ba183b4dfbf17a8fc6b
Celso Serrano Araujo <celso.saraujo@hotmail.com>
2023-05-30 22:25:23 -0300

    melhoria da legibilidade do código.

--- exemploSintaxe07.dart
+++ exemploSintaxe07.dart
@@ -11,13 +11,21 @@ void main() {
 }
 
 int divideNumeros(dividendo, divisor) {
+
     if( dividendo is! int || divisor is! int){
+
       throw Exception('O dividendo e o divisor devem ser um número inteiro válido!!!');
+
     }else if( divisor == 0 ) {
+
       throw Exception('O divisor deve ser diferente de Zero!!');
+
     }else{
+
        return dividendo ~/ divisor; 
+       
     }
+
 }
 
 
